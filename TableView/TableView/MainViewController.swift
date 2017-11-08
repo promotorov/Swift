73d266a03e717c59185de44483bb8b03fd7608af
promotorov <vladpromotorov@gmail.com>
2017-11-08 21:08:13 +0100

    Замена textFieldDayOfBirth на DatePicker
    Добавлено представление формата даты в textFieldDayOfBirth

--- TableView/TableView/MainViewController.swift
+++ TableView/TableView/MainViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITextFieldDelegate {
+    
+    var mainView: MainView{
+        get{
+            return view as! MainView
+        }
+    }
     override func loadView() {
         view = MainView()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainView.textFieldDateOfBirth.delegate = self
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let datePicker = UIDatePicker()
+        textField.inputView = datePicker
+        datePicker.addTarget(self, action: #selector(datePickerChanged), for: .valueChanged)
+    }
+    
+    @objc func datePickerChanged(sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        mainView.textFieldDateOfBirth.text = formatter.string(from: sender.date)
     }
 }
