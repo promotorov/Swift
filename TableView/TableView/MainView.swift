ef93d8335696927ec26eeac026b6289834679927
promotorov <vladpromotorov@gmail.com>
2017-11-08 20:06:51 +0100

    Добавлена форма
    Добавлена форма, содержащая три textField'a и кнопку

---
+++ TableView/TableView/MainView.swift
@@ -0,0 +1,61 @@
+import Foundation
+import UIKit
+
+class MainView: UIView {
+    var textFieldFirstName: UITextField = {
+        var textField = UITextField()
+        setTextFieldProperties(textField: textField)
+        textField.textColor = UIColor.black
+        textField.placeholder = "First Name"
+        return textField
+    }()
+    
+    var textFieldSecondName: UITextField = {
+        var textField = UITextField()
+        setTextFieldProperties(textField: textField)
+        textField.textColor = UIColor.black
+        textField.placeholder = "Second Name"
+        return textField
+    }()
+    
+    var textFieldDateOfBirth: UITextField = {
+        var textField = UITextField()
+        setTextFieldProperties(textField: textField)
+        textField.textColor = UIColor.black
+        textField.placeholder = "Date of Birthday"
+        return textField
+    }()
+    
+    var buttonAdd: UIButton = {
+        var button = UIButton()
+        button.setTitle("Add", for: .normal)
+        button.backgroundColor = UIColor.brown
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(textFieldFirstName)
+        addSubview(textFieldSecondName)
+        addSubview(textFieldDateOfBirth)
+        addSubview(buttonAdd)
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            textFieldFirstName.frame = CGRect(x: 20, y: 50, width: 250, height: 30)
+            textFieldSecondName.frame = CGRect(x: 20, y: 100, width: 250, height: 30)
+            textFieldDateOfBirth.frame = CGRect(x: 20, y: 150, width: 250, height: 30)
+            buttonAdd.frame = CGRect(x: 20, y: 200, width: 100, height: 50)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init has not been implemented")
+    }
+    
+    private static func setTextFieldProperties(textField: UITextField) {
+        textField.borderStyle = UITextBorderStyle.roundedRect
+        textField.textAlignment = NSTextAlignment.center
+    }
+}
---
+++ TableView/TableView/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  TableView
+//
+//  Created by Гость on 08.11.2017.
+//  Copyright © 2017 Гость. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func loadView() {
+        view = MainView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
